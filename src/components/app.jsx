c4ba2bf647d2a1ed26f68fae7f1094ec99179cdb
Aaron Kurowski <aaron12342@att.net>
2021-06-16 15:42:47 -0500

    Added ProductDetail folder with some preliminary code.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
 
-    
     render(){
         return(
-            <div>
-                <h1>Hello</h1>
-            </div>
+            <React.Fragment>          
+                <div>
+                    <h1>Hello</h1>
+                </div>     
+            </React.Fragment>
         );
     }
 }
