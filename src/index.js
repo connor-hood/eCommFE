ef3ed55781b9b95047c762f9e3b9a26a249e46e7
Aaron Kurowski <aaron12342@att.net>
2021-06-16 13:33:20 -0500

    App connected to index.js and configured folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
 
 const jsxElement = <h1>Tantalum Games</h1>;
 console.log(jsxElement);
 
-ReactDOM.render(jsxElement, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
