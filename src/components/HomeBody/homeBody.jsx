3cbf8bd6f822f9d03880aad58b259132d15ec651
DanielPichette <daniel.f.pichette@gmail.com>
2021-06-17 11:11:10 -0500

    updated the main app.css. created card base

--- src/components/HomeBody/homeBody.jsx
+++ src/components/HomeBody/homeBody.jsx
@@ -9,16 +9,33 @@ class HomeBody extends Component {
     render() { 
         return ( 
             <div className="HomeWrapper">
-                <div>product</div>
-                <div>product</div>
-                <div>product</div>
-                <div>product</div>
-                <div>product</div>
-                <div>product</div>
-                <div>product</div>
-                <div>product</div>
-                <div>product</div>
-                <div>product</div>
+                <div className="productCard">
+                    <h3>Name</h3>
+                    <div className="product image">
+                        <img src="https://via.placeholder.com/250" alt=""/>
+                    </div>
+                    <div className= "productInfo">
+                        <ul>
+                            <li>product Description</li>
+                            <li>Price</li>
+                        </ul>
+                        <a href="#"> Product detal</a>
+                        
+                    </div>
+                </div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                <div className="productCard">product</div>
+                
             </div>
          );
     }
