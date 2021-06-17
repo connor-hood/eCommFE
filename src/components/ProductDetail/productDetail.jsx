ddf3c88f2ccd3485620d6559d7c1d188ec685027
Aaron Kurowski <aaron12342@att.net>
2021-06-17 10:26:48 -0500

    Added api calls and state variables to app.jsx

--- src/components/ProductDetail/productDetail.jsx
+++ src/components/ProductDetail/productDetail.jsx
@@ -5,19 +5,7 @@ const ProductDetail = () => {
 
     // axios requests below will go in app.jsx but just hashing them out for now
 
-    // getAllProducts = async () => {
-    //     let query = "https://localhost:44394/api/product"
-    //     let allProducts = await axios.get(query);
-    //     this.setState({});
-    // }
-
-
-
-    // getProductById = async (product) => {
-    //     let query = `https://localhost:44394/api/product/${product.id}`;
-    //     let productById = await axios.get(query);
-    //     this.setSTate({});
-    // }
+    
 
     
 
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,22 +2,57 @@ import React, {Component} from 'react';
 import './app.css';
 import SearchBar from './Searchbar/searchbar';
 import NavBar from './NavBar/navBar';
+import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
         super(props);
+        debugger;
+        this.getAllProducts();
         this.state = {
-            currentUser: "Null",
-            allItems: "Null",
-            allComments: "Null",
+            currentUser: null,
+            allProducts: [],
+            selectedProduct: null,
+            userCart: null,
+
 
         }
     }
+
+    getAllProducts = async () => {
+        let query = "https://localhost:44394/api/product"
+        let response = await axios.get(query);
+        this.setState({
+            allProducts: response.data
+        });
+        debugger;
+        console.log(this.state.allProducts);
+    }
+
+    getProductById = async (product) => {
+        let query = `https://localhost:44394/api/product/${product.id}`;
+        let productById = await axios.get(query);
+        this.setSTate({});
+    }
+
+    deleteProduct = async (product) => {
+        let query = `https://localhost:44394/api/product/${product.id}`
+        let response = axios.delete(query);
+
+        this.getAllProducts();
+    }
+
+    updateProduct = async (product) => {
+
+    }
+
+
 
 
 
     render(){
         return(
+
             <div className='MainWrapper'>
                 <h1>Tantalum Games</h1>
                 <div className='NavBar'>
