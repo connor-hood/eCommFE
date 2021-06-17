970a363cc69a070d6978a0b777c4ac0ecdc5629e
connor-hood <connor_hood@yahoo.com>
2021-06-17 14:57:44 -0500

    login register updated with jwtdecode

---
+++ src/components/LoginRegister/loginRegister.jsx
@@ -0,0 +1,48 @@
+import jwtDecode from 'jwt-decode';
+import React, { Component } from 'react';
+
+class LoginRegister extends Component {
+    state = { 
+        userName: '',
+        password: ''
+    }
+    componentDidMount() {
+        const jwt = localStorage.getItem('token');
+        try{
+            const user = jwtDecode(jwt);
+            this.setState({
+                user
+            });
+        } catch {
+            
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
+    handleSubmit = (event) => {
+        event.preventOrDefault();
+
+    }
+    render() { 
+        return ( 
+            <div>
+                <form onSubmit={(event) => this.handleSubmit(event)}>
+                    <label>User Name</label>
+                    <input type="text" name="userName" onChange={this.handleChange} value={this.state.userName}/>
+                    <br></br>
+                    <label>Password</label>
+                    <input type="password" name="password" onChange={this.handleChange} value={this.state.password}/>
+                    <br></br>
+                    <button type="submit">Register</button>
+                    
+                </form>
+            </div>
+         );
+    }
+}
+ 
+export default LoginRegister;
