9c696ff2d016d92611c77071f5e5ea39bcfdd383
Aaron Kurowski <aaron12342@att.net>
2021-06-20 19:21:32 -0500

    Reviews are being mapped to the product detail page

--- src/components/ProductDetail/productDetail.jsx
+++ src/components/ProductDetail/productDetail.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './productDetail.css';
 
 const ProductDetail = (props) => {
 
-    if(props.selectedProduct !== null){
+    const [reviews, setReviews] = useState(null);
+
+    useEffect(() => {
+        getReviews(props.selectedProduct.productId);
+    }, []);
+    
+
+    const getReviews = async (productId) => {
+        let query = `https://localhost:44394/api/review/${productId}`
+        try{
+            let response = await axios.get(query);
+            console.log(response);
+            setReviews(response.data);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    if(reviews !== null){
+        debugger;
         return(
-            <div>
-                <img src={props.selectedProduct.imageURL} alt="placeholder" height="500" width="400"/>
-                <p>Name: {props.selectedProduct.name}</p>
-                <p>Description: {props.selectedProduct.description}</p>
-                <p>Type: {props.selectedProduct.type}</p>
-                <p>Age Rating: {props.selectedProduct.ageRating}</p>
-                <p>Genre: {props.selectedProduct.genre}</p>
-                <p>Price: ${props.selectedProduct.price}</p>
-            </div>
+            <React.Fragment>
+                <div>
+                    <img src={props.selectedProduct.imageURL} alt="placeholder" height="500" width="400"/>
+                    <p>Name: {props.selectedProduct.name}</p>
+                    <p>Description: {props.selectedProduct.description}</p>
+                    <p>Type: {props.selectedProduct.type}</p>
+                    <p>Age Rating: {props.selectedProduct.ageRating}</p>
+                    <p>Genre: {props.selectedProduct.genre}</p>
+                    <p>Price: ${props.selectedProduct.price}</p>
+                </div>
+                <div></div>
+                <div></div>
+                <div>
+                    {reviews.map((review) => 
+                        <div>{review.text}</div>
+                    )}
+                </div>
+                <div>
+                    Write a review...
+                </div>
+            </React.Fragment>
         );
     }
     else{
-        return("Uh oh...error");
+        return(
+            <h2>Loading Content...</h2>
+        );
     }
 }
 
