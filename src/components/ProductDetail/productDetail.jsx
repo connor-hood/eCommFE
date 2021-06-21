568e9c5c3696fd66f53d4f5dc04d5fd117a75c42
DanielPichette <daniel.f.pichette@gmail.com>
2021-06-21 13:37:55 -0500

    added css to the product details page

--- src/components/ProductDetail/productDetail.jsx
+++ src/components/ProductDetail/productDetail.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './productDetail.css';
-import Reviews from '../Reviews/reviews.jsx';
+//import Reviews from '../Reviews/reviews.jsx';
 
 const ProductDetail = (props) => { 
     return(
