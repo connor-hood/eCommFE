a408a6e6614d82d3cd1abc5690908f515f85fe7e
connor-hood <connor_hood@yahoo.com>
2021-06-21 09:09:08 -0500

    added add to cart button on detail page

--- src/components/ProductDetail/productDetail.jsx
+++ src/components/ProductDetail/productDetail.jsx
@@ -15,6 +15,7 @@ const ProductDetail = (props) => {
                 <p>Age Rating: {props.selectedProduct.ageRating}</p>
                 <p>Genre: {props.selectedProduct.genre}</p>
                 <p>Price: ${props.selectedProduct.price}</p>
+                <button>Add to Cart!</button>
             </div>
 
             <br></br>
